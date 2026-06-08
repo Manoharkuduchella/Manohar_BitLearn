0bea4db90523c2c2dc335948a8a3d80d812d00f2
man <manohar159122@gmail.com>
2026-06-08 10:50:31 +0530

    Remove old STM32 LL projects

--- Bitlearn_workspace/Microcontrollers/STM32/stm32l4_workspace_LL/05_I2ctest/Debug/Drivers/Src/stm32l476xx_i2c_drivers.d
+++
@@ -1,6 +0,0 @@
-Drivers/Src/stm32l476xx_i2c_drivers.o: \
- ../Drivers/Src/stm32l476xx_i2c_drivers.c \
- C:/Users/manohar\ kuduchella/STM32CubeIDE/stm32l4_workspace_LL/I2ctest/Drivers/Inc/stm32l476xx_i2c_drivers.h \
- C:/Users/manohar\ kuduchella/STM32CubeIDE/stm32l4_workspace_LL/I2ctest/Drivers/Inc/stm32l476xx.h
-C:/Users/manohar\ kuduchella/STM32CubeIDE/stm32l4_workspace_LL/I2ctest/Drivers/Inc/stm32l476xx_i2c_drivers.h:
-C:/Users/manohar\ kuduchella/STM32CubeIDE/stm32l4_workspace_LL/I2ctest/Drivers/Inc/stm32l476xx.h:
